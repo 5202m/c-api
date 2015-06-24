8b57d27a78a31a392e821d28a21d4e07eeb7dad7
alan <alan@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-06-24 02:06:42 +0000

    调整代码
    git-svn-id: svn://172.30.3.30/pm/pm_api/trunk@699 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- cache/cacheClient.js
+++ cache/cacheClient.js
@@ -1,11 +1,12 @@
+var config=require("../resources/config");
 /**
  * 定义缓存连接的客户端
  * @type {exports}
  */
 var redis = require("redis"),//引入redis
-    cacheClient = redis.createClient(6379,'192.168.35.236',{});//连接redis
+    cacheClient = redis.createClient(config.redisUrlObj.port,config.redisUrlObj.host,{});//连接redis
     cacheClient.on("error", function (err) { //错误监听
-        console.log("Error " + err);
+        console.log("connect to redis has error:" + err);
     });
 module.exports =cacheClient;
 
