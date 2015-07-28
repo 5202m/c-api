b46e84f9bbc46b7a239b7a06e6e79f66134243b7
alan <alan@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-07-28 11:06:43 +0000

    调整代码
    git-svn-id: svn://172.30.3.30/pm/pm_api/trunk@1014 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- models/media.js
+++ models/media.js
@@ -1,27 +1,26 @@
 /**
- * 摘要：文章资讯实体类 (主要用于查询)
- * author: Gavin.guo
- * date: 2015/4/23
+ * 摘要：媒体实体类
+ * author:Alan.wu
+ * date: 2015/7/3
  */
 var mongoose = require('mongoose')
     , Schema = mongoose.Schema
-    , articleSchema = new Schema({
+    ,mediaSchema = new Schema({
         _id : String,
         categoryId: {type:String,index:true} ,   /**栏目*/
         status: {type:Number, default:1}, /**状态*/
-        platform : {type:String,index:true},      /**应用平台*/
+        platform : {type:String,index:true},  /**应用平台*/
         createDate : Date,       /**创建时间*/
         publishStartDate: {type:Date,index:true},
         publishEndDate:{type:Date,index:true},
-        valid:{type:Number, default:1}, /**是否有效*/
-        sequence: {type:Number, default:1},
         mediaUrl:String,/**媒体地址路径*/
         mediaImgUrl:String,/** 媒体图片（视频专用字段）*/
         linkUrl:String,/** 点击媒体链接的路径*/
-        detailList : [{     /**文章资讯详细信息*/
+        valid:{type:Number, default:1}, /**是否有效*/
+        detailList : [{   /**文章资讯详细信息*/
             lang: {type:String,index:true} ,          /**语言*/
             title: String ,         /**标题*/
-            content:String         /**内容*/
-        }]  /**文章资讯详细信息*/
+            remark:String         /**简介*/
+        }]
     });
-module.exports = mongoose.model('article',articleSchema,'article');
+module.exports = mongoose.model('media',mediaSchema,'media');
