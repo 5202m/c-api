a86336c50acd9a0a0ecea3f1fb6697cfaee02e3b
alan <alan@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-10-14 08:11:14 +0000

    调整日志代码
    git-svn-id: svn://172.30.3.30/pm/pm_api/trunk@1741 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- routes/api/smsAPI.js
+++ routes/api/smsAPI.js
@@ -14,25 +14,24 @@ var config = require('../../resources/config');
  * 发送短信
  */
 router.get('/send', function(req, res) {
-	var mobile = req.param("mobile");
+	var mobile = req.query["mobile"];
 	if(common.isBlank(mobile)){
 		res.json(errorMessage.code_1000);
         return;
 	}
 	var smsUrl = config.smsUrl;
-	var content = req.param("content");
+	var content = req.query["content"];
 	if(common.isBlank(content)){   //如果不传入内容，则默认是短信验证码，随机输入6位
 		content = common.randomNumber(6);
 		smsUrl = smsUrl + "/sms_send.ucs?type=AUTH_CODE"+"&PHONE="+mobile+"&CODE="+content;
 	}else{    //如果传入内容，则按内容输出
 		smsUrl = smsUrl + "/sms_send_common.ucs?phone="+mobile+"&content="+content;
 	}
-	console.info(smsUrl);
 	request(smsUrl,function(error, response, data){
 		if (!error && response.statusCode == 200 && common.isValid(data)) {
 			res.json({result:0,content : content});
 		}else{
-            console.error("smsAPI->sendSms has error:"+error);
+            console.error("smsAPI["+smsUrl+"]->sendSms has error:"+error);
 			res.json({result:1,errCode:errorMessage.code_1002.errcode,errMessage:errorMessage.code_1002.errmsg});
 		}
 	});
