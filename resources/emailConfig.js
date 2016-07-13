31a08e9306fb3cb4e237f16c9df6ec54a6a3546f
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-07-13 07:17:07 +0000

    提取邮箱发送配置，区分UAT和REAL
    git-svn-id: svn://172.30.3.30/pm/pm_api/trunk@3672 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

---
+++ resources/emailConfig.js
@@ -0,0 +1,72 @@
+/**
+ * 邮件配置信息
+ */
+var emailConfig = {
+    /** 邮件发送服务器配置 */
+    configs : {
+        //PM
+        studio : {
+            server :{
+                host: "smtp.qq.com",
+                secure: true,
+                port: 465,
+                auth: {
+                    user: "2807001686@qq.com",
+                    pass: "opkyukikffwfdfed"
+                }
+            },
+            from : "PM直播间<2807001686@qq.com>",
+            to : "370419450@qq.com",//"expert@24k.hk",
+            subject: function(data){
+                return "[PM直播间]专家邮箱:" + (data.email || "");
+            },
+            template : "email/studio"
+        },
+        //FX
+        fxstudio : {
+            server :{
+                host: "smtp.qq.com",
+                secure: true,
+                port: 465,
+                auth: {
+                    user: "2807001686@qq.com",
+                    pass: "opkyukikffwfdfed"
+                }
+            },
+            from : "FX直播间<2807001686@qq.com>",
+            to : "370419450@qq.com",//"cs@gwfx.com",
+            subject: function(data){
+                return "[FX直播间]专家邮箱:" + (data.email || "");
+            },
+            template : "email/fxstudio"
+        },
+        //HX
+        hxstudio : {
+            server :{
+                host: "smtp.qq.com",
+                secure: true,
+                port: 465,
+                auth: {
+                    user: "2807001686@qq.com",
+                    pass: "opkyukikffwfdfed"
+                }
+            },
+            from : "HX直播间<2807001686@qq.com>",
+            to : "370419450@qq.com",//"expert@hx9999.com",
+            subject: function(data){
+                return "[HX直播间]专家邮箱:" + (data.email || "");
+            },
+            template : "email/hxstudio"
+        }
+    },
+    /**获取配置*/
+    get : function(key){
+        if(!key || !this.configs.hasOwnProperty(key)){
+            return null;
+        }
+        return this.configs[key];
+    }
+};
+//导出配置
+module.exports = emailConfig;
+
--- service/emailService.js
+++ service/emailService.js
@@ -12,63 +12,10 @@
  */
 var Logger = require('../resources/logConf').getLogger("emailService");
 var Nodemailer = require("nodemailer");
+var EmailConfig = require('../resources/emailConfig');
 var ApiResult = require('../util/ApiResult');
 
 var emailService = {
-    /** 邮件发送服务器配置 */
-    configs : {
-        studio : {
-            server :{
-                host: "smtp.qq.com",
-                secure: true,
-                port: 465,
-                auth: {
-                    user: "2807001686@qq.com",
-                    pass: "opkyukikffwfdfed"
-                }
-            },
-            from : "PM直播间<2807001686@qq.com>",
-            to : "expert@24k.hk",
-            subject: function(data){
-                return "[PM直播间]专家邮箱:" + (data.email || "");
-            },
-            template : "email/studio"
-        },
-        fxstudio : {
-            server :{
-                host: "smtp.qq.com",
-                secure: true,
-                port: 465,
-                auth: {
-                    user: "2807001686@qq.com",
-                    pass: "opkyukikffwfdfed"
-                }
-            },
-            from : "FX直播间<2807001686@qq.com>",
-            to : "cs@gwfx.com",
-            subject: function(data){
-                return "[FX直播间]专家邮箱:" + (data.email || "");
-            },
-            template : "email/fxstudio"
-        },
-        hxstudio : {
-            server :{
-                host: "smtp.qq.com",
-                secure: true,
-                port: 465,
-                auth: {
-                    user: "2807001686@qq.com",
-                    pass: "opkyukikffwfdfed"
-                }
-            },
-            from : "HX直播间<2807001686@qq.com>",
-            to : "expert@hx9999.com",
-            subject: function(data){
-                return "[HX直播间]专家邮箱:" + (data.email || "");
-            },
-            template : "email/hxstudio"
-        }
-    },
     /**邮件发送器缓存*/
     transporters : {},
 
@@ -79,11 +26,11 @@ var emailService = {
      * @param callback
      */
     send : function(key, data, callback){
-        if(!key || !emailService.configs.hasOwnProperty(key)){
+        var config = EmailConfig.get(key);
+        if(!config){
             callback(ApiResult.result("邮件配置信息不存在：" + key, false));
             return;
         }
-        var config = emailService.configs[key];
         var mailOps = {
             from : config.from,
             to : config.to,
