54edef4929ba39e7e4e5241428fc28dbd88a9afd
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2016-08-26 10:13:26 +0000

    增加手机号码查询用户信息接口
    git-svn-id: svn://172.30.3.30/pm/pm_api/trunk@4160 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- routes/api/chatAPI.js
+++ routes/api/chatAPI.js
@@ -8,6 +8,7 @@ var router = express.Router();
 var common = require('../../util/common');
 var errorMessage = require('../../util/errorMessage.js');
 var chatService = require('../../service/chatService');
+var userService = require('../../service/userService');
 var ApiResult = require('../../util/ApiResult');
 
 /**
@@ -48,6 +49,23 @@ router.post("/checkChatPraise", function(req, res) {
     }else{
         chatService.checkChatPraise(clientId,praiseId,fromPlatform,function(isOK){
             res.json({result:isOK});
+        });
+    }
+});
+
+/**
+ * 按照手机号查询客户信息
+ */
+router.get("/getMemberByMobile", function(req, res) {
+    var params = {
+        groupType : req.query["groupType"],
+        mobilePhone : req.query["mobilePhone"]
+    };
+    if(common.isBlank(params.groupType)||common.isBlank(params.mobilePhone)){
+        res.json(null);
+    }else{
+        userService.getMemberByMobile(params, function(err, member){
+            res.json(member);
         });
     }
 });
--- service/userService.js
+++ service/userService.js
@@ -1,11 +1,11 @@
 /** 用户服务类
  * Created by Alan.wu on 2015/3/4.
  */
+var logger = require('../resources/logConf').getLogger("userService");
 var member = require('../models/member');//引入member数据模型
 
 /**
  * 定义用户服务类
- * @type {{getMemberList: Function, updateMemberInfo: Function}}
  */
 var userService = {
     /**
@@ -17,6 +17,58 @@ var userService = {
                 callback(null);
             }
             callback(members);
+        });
+    },
+
+    /**
+     * 按照手机号查询用户信息
+     * @param params  {{mobilePhone:String, groupType:String}}
+     * @param callback
+     */
+    getMemberByMobile: function(params, callback){
+        member.findOne({
+            valid:1,
+            status:1,
+            mobilePhone:params.mobilePhone,
+            "loginPlatform.chatUserGroup._id":params.groupType
+        }, {
+            "mobilePhone": 1,
+            "loginPlatform.chatUserGroup.$": 1
+        }, function(err, data){
+            if(err || !data){
+                if(err){
+                    logger.error("getMemberByMobile>>get momber info error:"+ err);
+                }
+                callback(err, null);
+                return;
+            }
+            var result = {
+                mobilePhone : data.mobilePhone
+            };
+            if(data.loginPlatform && data.loginPlatform.chatUserGroup && data.loginPlatform.chatUserGroup.length > 0){
+                var chatUserGroup = data.loginPlatform.chatUserGroup[0];
+                result.groupType = chatUserGroup._id;
+                result.userId = chatUserGroup.userId;
+                result.avatar = chatUserGroup.avatar;
+                result.nickname = chatUserGroup.nickname;
+                result.userType = chatUserGroup.userType;
+                result.vipUser = chatUserGroup.vipUser;
+                result.clientGroup = chatUserGroup.clientGroup;
+                result.createDate = (chatUserGroup.createDate instanceof Date ? chatUserGroup.createDate.getTime() : 0);
+                var rooms = [], room;
+                for(var i = 0, lenI = chatUserGroup.rooms ? chatUserGroup.rooms.length : 0; i < lenI; i++){
+                    room = chatUserGroup.rooms[i];
+                    rooms.push({
+                        roomId : room._id,
+                        onlineStatus : room.onlineStatus,
+                        sendMsgCount : room.sendMsgCount,
+                        onlineDate : (room.onlineDate instanceof Date ? room.onlineDate.getTime() : 0),
+                        offlineDate : (room.offlineDate instanceof Date ? room.offlineDate.getTime() : 0)
+                    });
+                }
+                result.rooms = rooms;
+            }
+            callback(null, result);
         });
     }
 };
