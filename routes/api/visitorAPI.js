6345934b5b91e748048cc85804b851c6d771421a
Eugene.Zeng <eugene.zeng@gwtsz.net>
2017-03-01 18:08:36 +0800

    Fixed issue in getVisitorByName, added saveVisitorRecord.

--- routes/api/visitorAPI.js
+++ routes/api/visitorAPI.js
@@ -1,5 +1,5 @@
 "use strict";
-const logger =require("../../resources/logConf").getLogger("visitorAPI");
+const logger = require("../../resources/logConf").getLogger("visitorAPI");
 const router = require('express').Router();
 const visitorService = require('../../service/visitorService');
 const common = require('../../util/common');
@@ -12,7 +12,7 @@ router.post("/saveVisitorRecord", (req, res) => {
     let isSatify = requires.every((name) => {
         return common.isValid(req.body[name]);
     });
-    if(!isSatify){
+    if (!isSatify) {
         logger.warn("[saveVisitorRecord] Parameters missed! Expecting parameters: ", requires);
         res.json(APIUtil.APIResult("code_1000", null));
         return;
@@ -21,7 +21,7 @@ router.post("/saveVisitorRecord", (req, res) => {
     let dasData = JSON.parse(req.body.dasData);
     visitorService.saveVisitorRecord(type, dasData)
         .then(result => {
-            res.json(APIUtil.APIResult(null, {isOK: true}));
+            res.json(APIUtil.APIResult(null, { isOK: true }));
         })
         .catch(err => {
             logger.warn("visitorService.saveVisitorRecord fail!", err);
@@ -32,16 +32,16 @@ router.post("/saveVisitorRecord", (req, res) => {
 router.get("/getVistiorByName", (req, res) => {
     let requires = ["groupType", "roomId", "nickname"];
     let isSatify = requires.every((name) => {
-        return common.isValid(req.req[name]);
+        return common.isValid(req.query[name]);
     });
-    if(!isSatify){
+    if (!isSatify) {
         logger.warn("[getVistiorByName] Parameters missed! Expecting parameters: ", requires);
         res.json(APIUtil.APIResult("code_1000", null));
         return;
     }
-    let groupType = req.req.groupType;
-    let roomId = req.req.roomId;
-    let nickname = req.req.nickname;
+    let groupType = req.query.groupType;
+    let roomId = req.query.roomId;
+    let nickname = req.query.nickname;
     visitorService.getVistiorByName(groupType, roomId, nickname)
         .then(data => {
             res.json(APIUtil.APIResult(null, data));
