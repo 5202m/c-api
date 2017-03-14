5f6a8703e6f01507563e22d353e3fd319e1a49c8
Eugene.Zeng <eugene.zeng@gwtsz.net>
2017-03-14 13:39:47 +0800

    Caught the exception for JSON.parse and JSON.stringify.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var logger = require('./resources/logConf');
-var config=require('./resources/config');
+var logConf = require('./resources/logConf');
+var config = require('./resources/config');
 /*＃＃＃＃＃＃＃＃＃＃引入所需插件＃＃＃＃＃＃＃＃end */
 
 /*＃＃＃＃＃＃＃＃＃＃定义app配置信息＃＃＃＃＃＃＃＃begin */
@@ -13,18 +13,18 @@ var app = express();
 // view engine setup(定义页面，使用html）
 app.set('views', path.join(__dirname, 'views'));
 /*app.set('view engine', 'ejs');*/
-app.set( 'view engine', 'html' );
-app.engine('.html',require('ejs').__express);//两个下划线
-logger.initConfig();
-logger.use(app);//配置框架日志输出
-app.use(bodyParser.json({limit: '100mb'}));
-app.use(bodyParser.urlencoded({limit: "100mb", extended: true, parameterLimit:50000}));
+app.set('view engine', 'html');
+app.engine('.html', require('ejs').__express); //两个下划线
+logConf.initConfig();
+logConf.use(app); //配置框架日志输出
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(path.join(__dirname, 'views')));如需要设成静态目录，则这就去掉注释。（备注：设为静态目录，不能动态填充数据）
 
 /*＃＃＃＃＃＃＃＃＃＃路由入口设置＃＃＃＃＃＃＃＃begin */
-var index = require('./routes/index').init(app);//配置同源页面路由
+var index = require('./routes/index').init(app); //配置同源页面路由
 
 // catch 404 and forward to error handler （400请求错误处理）
 app.use(function(req, res, next) {
@@ -60,11 +60,33 @@ app.use(function(err, req, res, next) {
 
 /*＃＃＃＃＃＃＃＃＃＃数据库连接配置＃＃＃＃＃＃＃＃begin */
 var dboptions = {
-    server: {auto_reconnect: true, poolSize: 5 },
+    server: { auto_reconnect: true, poolSize: 5 },
     user: config.dbUserName,
     pass: config.dbUserPWD
 };
-mongoose.connect(config.dbURL,dboptions);
+mongoose.connect(config.dbURL, dboptions);
 /*＃＃＃＃＃＃＃＃＃＃数据库连接配置＃＃＃＃＃＃＃＃end */
+
+(function(JSON) {
+    let logger = logConf.getLogger("application");
+    let jParse = JSON.parse;
+    JSON.parse = function() {
+        try {
+            return jParse.apply(JSON, arguments);
+        } catch (e) {
+            logger.error(`JSON parse Error in ${arguments.callee.name}: `, e);
+            return {};
+        };
+    };
+    let jStringify = JSON.stringify;
+    JSON.stringify = function() {
+        try {
+            return jStringify.apply(JSON, arguments);
+        } catch (e) {
+            logger.error(`JSON stringify Error in ${arguments.callee.name}: `, e);
+            return "";
+        }
+    };
+})(global.JSON);
 
 module.exports = app;
