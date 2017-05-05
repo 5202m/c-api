3ec55dd1409db300fcfce2c9e7367cb2af726d2c
Eugene.Zeng <eugene.zeng@gwtsz.net>
2017-05-05 11:47:19 +0800

    Added logger information once redis ready.

--- cache/cacheClient.js
+++ cache/cacheClient.js
@@ -1,12 +1,16 @@
 var logger = require('../resources/logConf').getLogger("redis");
-var config=require("../resources/config");
+var config = require("../resources/config");
 /**
  * 定义缓存连接的客户端
  * @type {exports}
  */
-var redis = require("redis"),//引入redis
-    cacheClient = redis.createClient(config.redisUrlObj.port,config.redisUrlObj.host,{});//连接redis
-    cacheClient.on("error", function (err) { //错误监听
-    	logger.info("connect to redis has error:" + err);
+var redis = require("redis"), //引入redis
+    cacheClient = redis.createClient(config.redisUrlObj.port, config.redisUrlObj.host, {}); //连接redis
+
+cacheClient
+    .on("ready", () => {
+        logger.info(`Connected to redis ready: ${config.redisUrlObj.host}:${config.redisUrlObj.port}`);
+    }).on("error", function(err) { //错误监听
+        logger.info("Connect to redis has error:" + err);
     });
-module.exports =cacheClient;
+module.exports = cacheClient;
