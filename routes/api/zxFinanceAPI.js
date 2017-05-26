3ae1ab2f7d11c1aebc835b1fb324dc042b0f626a
Jade.zhu <jade.zhu@gwtsz.net>
2017-05-26 16:58:09 +0800

    规范apidoc写法

--- routes/api/zxFinanceAPI.js
+++ routes/api/zxFinanceAPI.js
@@ -56,9 +56,9 @@ let common = require('../../util/common');
  *          "errcode": "0",
  *          "errmsg": "",
  *          "data": {
- *          	financeEvent: [ ], //财经事件
- *              financeVacation: [ ], //假期预告
- *              financeData: [ ]  //财经日历
+ *          	"financeEvent": [ ], //财经事件
+ *            "financeVacation": [ ], //假期预告
+ *            "financeData": [ ]  //财经日历
  *          }
  *      }
  *
