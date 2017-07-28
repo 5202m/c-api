0fd0e636c1e48ff0e27ce23e08fb3f2081550bee
Jade.zhu <jade.zhu@gwtsz.net>
2017-07-28 14:29:00 +0800

    还原参数值

--- service/showTradeService.js
+++ service/showTradeService.js
@@ -69,14 +69,14 @@ var showTradeService = {
     getShowTradeList: function(params, callback) {
         var searchObj = {
             "groupType": params.groupType,
-            "valid": 0,
+            "valid": 1,
             "status": 1,
             "tradeType": 2
         };
         if (common.isValid(params.userNo)) {
             searchObj = {
                 "groupType": params.groupType,
-                "valid": 0,
+                "valid": 1,
                 "tradeType": (params.tradeType ? params.tradeType : 2),
                 "boUser.userNo": params.userNo
             };
