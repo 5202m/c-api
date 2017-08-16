5076385f0639a0fb56a95694d0a23f91c13ab476
Eugene Zeng <euene.zeng@live.com>
2017-08-16 15:00:24 +0800

    Added stress testing

--- test/testPointsAPI.js
+++ test/testPointsAPI.js
@@ -1,59 +1,77 @@
 "use strict";
-let should = require('should');
-let request = require('request');
-let pointsAPI = require('../routes/api/pointsAPI');
-let common = require('../util/common'); //引入common类
-let logger = require('../resources/logConf').getLogger("testPointsAPI");
+const should = require('should');
+const request = require('request');
+const pointsAPI = require('../routes/api/pointsAPI');
+const tokenService = require('../service/tokenService');
+const qs = require('querystring');
+const async = require("async");
+const common = require('../util/common'); //引入common类
+const logger = require('../resources/logConf').getLogger("testPointsAPI");
 
 
-let data = {
-  groupType: 'studio',
-  userId: '13824390062',
-  hasJournal: true,
-  systemCategory: 'pm',
-  token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjaGF0UGxhdGZvcm0iLCJleHAiOjE1MDI3Njk3MTQ2NDd9.6544tcCjRxjYrU5a24dxC20LSrj2mm3DSu-cEv8a-nI',
-  appsecret:'3b970000'
-};
-let url = '/api/points';
-describe("pointsAPI.pointsInfo", () => {
-  it("Should work as expect.", (done) => {
-    let options = {
-      url: `${url}/pointsInfo`,
-      body: data,
-      json: true
+let url = 'http://localhost:3000/api';
+let testIds = "13783599511";
+describe("pointsAPI testing", function() {
+    this.timeout(50000);
+    let data = {
+        groupType: 'studio',
+        userId: '13824390062',
+        hasJournal: "0",
+        systemCategory: 'pm',
+        token: null,
+        appsecret: 'df1a0002'
     };
-    logger.info("posting data to ", options.url);
-    request.get(`${url}/pointsInfo`)
-    .send(data)
-    .expect(200, (err, res, data) => {
-      should.not.exist(err);
-      res.should.be.an.Object();
-      data.should.be.an.Object();
-      logger.info(JSON.stringify(data));
-      done();
+    before(function(done) {
+        let options = {
+            url: `${url}/token/getToken`,
+            method: "POST",
+            body: {
+                appId: "fx_chat",
+                appSecret: data.appsecret
+            },
+            json: true
+        };
+        request(options, function(err, res, result) {
+            logger.log(JSON.stringify(result));
+            data.token = result.token;
+            done();
+        });
     });
-  });
+    describe("pointsAPI.pointsInfo", () => {
+        it("Should work as expect.", (done) => {
+            let queryObj = {
+                groupType: data.groupType,
+                userId: data.userId,
+                systemCategory: data.systemCategory
+            };
+
+            let requestPoint = function(userId, callback) {
+                logger.log("Processing userId: ", userId);
+                queryObj.userId = userId;
+                let options = {
+                    url: `${url}/points/pointsInfo?${qs.stringify(queryObj)}`,
+                    headers: {
+                        'apptoken': data.token,
+                        'appsecret': data.appsecret
+                    },
+                    json: true
+                };
+                request(options, (err, res, data) => {
+                    should.not.exist(err);
+                    res.should.be.an.Object();
+                    data.should.be.an.Object();
+                    callback();
+                });
+            }
+            let userIds = testIds.split(",");
+            logger.info(`Got ${userIds.length} userIds...`);
+            async.each(userIds, requestPoint, function(err) {
+                if (err) {
+                    logger.error(err);
+                } else {
+                    done();
+                }
+            });
+        });
+    });
 });
-/*describe("studioAPI.login", () => {
-  it("Should work as expect.", (done) => {
-    let loginData = {
-      userInfo: { userId: 'vxcnnfsipssdi', groupType: 'hxstudio' },
-      type: 2
-    };
-    let options = {
-      url: `${url}/login`,
-      body: loginData,
-      json: true
-    };
-    logger.info("posting data to ", options.url);
-    request.post(`${url}/login`)
-    .send(loginData)
-    .expect(200, (err, res, data) => {
-      should.not.exist(err);
-      res.should.be.an.Object();
-      data.should.be.an.Object();
-      logger.info(JSON.stringify(data));
-      done();
-    });
-  });
-});*/
